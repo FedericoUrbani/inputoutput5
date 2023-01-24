8e4d63b8e64c6cc7da6ee5f74c53ac9753907010
Federico Urbani <urbanifederico5@gmail.com>
2023-01-24 21:50:47 +0100

    upload printyourage

---
+++ PrintYourAgeFromInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class PrintYourAgeFromInput {
+    public static void main (String args[]){
+    Scanner input=new Scanner(System.in);
+    int age = input.nextInt();
+
+    System.out.println(age);
+}
+}
